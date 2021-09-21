a674804a9d6a32a50d0e95a157370697968d9aa8
cedy <maulioncedric@gmail.com>
2021-09-21 10:30:21 +0000

    2nd commit modified

--- one.py
+++ one.py
@@ -1,9 +1,18 @@
-print("Enter Marks Obtained in 4 Subjects: ")
+print("Enter Marks Obtained in the whole se ")
+print("Prelim")
 pre = int(input())
+print("Midterm")
 mid = int(input())
+print("Semis")
 semis = int(input())
+print("Finals")
 fina = int(input())
 
 
 tot = pre + mid + semis + fina
 avg = tot/4
+
+if avg >=75:
+    print(" You Passed ")
+else :
+    print("You Failed")
